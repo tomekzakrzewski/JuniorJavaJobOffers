56842529933036046afd5d5e220d0035ae30ff0d
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2023-10-04 19:32:27 +0200

    implement base class for integration testing

--- src/integration/java/pl/zakrzewski/juniorjavajoboffers/BaseIntegrationTest.java
+++ src/integration/java/pl/zakrzewski/juniorjavajoboffers/BaseIntegrationTest.java
@@ -1,13 +1,37 @@
 package pl.zakrzewski.juniorjavajoboffers;
 
-import org.junit.jupiter.api.Test;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @SpringBootTest(classes = JuniorJavaJobOffersApplication.class)
+@ActiveProfiles("integration")
+@AutoConfigureMockMvc
+@Testcontainers
 public class BaseIntegrationTest {
+    public static final String WIRE_MOCK_HOST = "http://localhost";
+    @Autowired
+    public MockMvc mockMvc;
+    @Container
+    private static final MySQLContainer mySQLContainer = new MySQLContainer("mysql:8");
 
-    @Test
-    public void f() {
-
+    @RegisterExtension
+    public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
     }
 }
