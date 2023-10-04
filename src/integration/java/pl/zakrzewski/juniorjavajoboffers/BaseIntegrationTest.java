8e01a2834ca3fb5515cc85f985532e1298d01715
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2023-10-04 15:52:53 +0200

    add baseintegrationtest

--- src/integration/java/pl/zakrzewski/juniorjavajoboffers/BaseIntegrationTest.java
+++ src/integration/java/pl/zakrzewski/juniorjavajoboffers/BaseIntegrationTest.java
@@ -1,7 +1,13 @@
 package pl.zakrzewski.juniorjavajoboffers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = JuniorJavaJobOffersApplication.class)
 public class BaseIntegrationTest {
+
+    @Test
+    public void f() {
+
+    }
 }
