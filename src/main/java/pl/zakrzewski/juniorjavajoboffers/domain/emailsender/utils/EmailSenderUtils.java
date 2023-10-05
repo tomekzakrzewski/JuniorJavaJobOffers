67310af4a723f23693479d840a14099b6454e3ff
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2023-10-05 13:44:26 +0200

    refactor, delete unused method for creating message for email

--- src/main/java/pl/zakrzewski/juniorjavajoboffers/domain/emailsender/utils/EmailSenderUtils.java
+++ src/main/java/pl/zakrzewski/juniorjavajoboffers/domain/emailsender/utils/EmailSenderUtils.java
@@ -1,9 +1,6 @@
 package pl.zakrzewski.juniorjavajoboffers.domain.emailsender.utils;
 
 public class EmailSenderUtils {
-    public static String getEmailConfirmationMessage(String host, String token) {
-        return "Your account has been created. Please click the link below to verify your account. \n\n" + getVerificationUrl(host, token);
-    }
 
     public static String getVerificationUrl(String host, String token) {
         return host + "/api/v1/registration?token=" + token;
@@ -12,5 +9,4 @@ public class EmailSenderUtils {
     public static String getUnsubscribeUrl(String host, String id) {
         return host + "/api/v1/registration/unsubscribe?id=" + id;
     }
-
 }
