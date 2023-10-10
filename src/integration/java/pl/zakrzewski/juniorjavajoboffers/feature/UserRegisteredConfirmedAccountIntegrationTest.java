b771c8e404071ad8c79235c61d44796de9ae635d
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2023-10-10 22:36:28 +0200

    clean code, change steps in integration

--- src/integration/java/pl/zakrzewski/juniorjavajoboffers/feature/UserRegisteredConfirmedAccountIntegrationTest.java
+++ src/integration/java/pl/zakrzewski/juniorjavajoboffers/feature/UserRegisteredConfirmedAccountIntegrationTest.java
@@ -12,7 +12,6 @@ import pl.zakrzewski.juniorjavajoboffers.SampleOfferResponse;
 import pl.zakrzewski.juniorjavajoboffers.domain.offer.OfferFacade;
 import pl.zakrzewski.juniorjavajoboffers.domain.offer.OfferFetchable;
 import pl.zakrzewski.juniorjavajoboffers.domain.offer.dto.OfferDto;
-import pl.zakrzewski.juniorjavajoboffers.domain.offer.dto.OfferResponse;
 import pl.zakrzewski.juniorjavajoboffers.domain.offer.exceptions.NewOffersNotFound;
 import pl.zakrzewski.juniorjavajoboffers.domain.register.dto.RegisterRequestDto;
 import pl.zakrzewski.juniorjavajoboffers.domain.register.dto.RegisterResultDto;
@@ -39,7 +38,18 @@ public class UserRegisteredConfirmedAccountIntegrationTest extends BaseIntegrati
     @Test
     public void should_user_register_and_confirm_account_and_system_send_job_offers() throws Exception {
 
-        // step 1: user made POST to /register with username=Tomek and email=tomek@gmail.com and recived confirmation mail
+        // step 1: external service returns job offers
+        wireMockServer.stubFor(WireMock.post("/")
+                .withRequestBody(WireMock.matching(".*"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(bodyWithThreeOffersJson())
+                )
+        );
+
+
+        // step 2: user made POST to /register with username=Tomek and email=tomek@gmail.com and recived confirmation mail
         RegisterRequestDto request = new RegisterRequestDto("Tomek", "tomek@gmail.com");
         ResultActions successRegisterRequest = mockMvc.perform(post("/api/v1/registration")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -53,21 +63,10 @@ public class UserRegisteredConfirmedAccountIntegrationTest extends BaseIntegrati
         assertThat(greenMail.waitForIncomingEmail(TIMEOUT, 1)).isTrue();
 
 
-        // step 2: user made GET to /registration?token= with generated token and confirmed account
+        // step 3: user made GET to /registration?token= with generated token and confirmed account
         ResultActions successConfirmToken = mockMvc.perform(get("/api/v1/registration?token=" + token)
                 .contentType(MediaType.APPLICATION_JSON));
         MvcResult confirmTokenResult = successConfirmToken.andExpect(status().isOk()).andReturn();
-
-
-        // step 3: external service returns job offers
-        wireMockServer.stubFor(WireMock.post("/")
-                .withRequestBody(WireMock.matching(".*"))
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(bodyWithThreeOffersJson())
-                )
-        );
 
 
         // step 4: system fetches job offers at given time
@@ -81,19 +80,18 @@ public class UserRegisteredConfirmedAccountIntegrationTest extends BaseIntegrati
                                 return false;
                             }
                         }
-                        );
+                );
+
 
         // step 5: system sends email with job offers to user at given time
         assertThat(greenMail.waitForIncomingEmail(TIMEOUT, 2)).isTrue();
 
 
-        //step  : user made POST to /unsubscribe with ID and unsubscribed
+        //step 6: user made POST to /unsubscribe with ID and unsubscribed
         String userId = registerResultDto.id();
         ResultActions successUnsubscribe = mockMvc.perform(post("/api/v1/registration/unsubscribe?id=" + userId)
                 .contentType(MediaType.APPLICATION_JSON));
 
         successUnsubscribe.andExpect(status().isNoContent());
-
-
     }
 }
