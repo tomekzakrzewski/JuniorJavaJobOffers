bd822a72372a8fd10176f5ae497c81e945eda9a7
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2023-10-10 22:37:06 +0200

    remove whitespace

--- src/main/java/pl/zakrzewski/juniorjavajoboffers/domain/emailsender/utils/EmailSenderUtils.java
+++ src/main/java/pl/zakrzewski/juniorjavajoboffers/domain/emailsender/utils/EmailSenderUtils.java
@@ -5,7 +5,6 @@ public class EmailSenderUtils {
     public static String getVerificationUrl(String host, String token) {
         return host + "/api/v1/registration?token=" + token;
     }
-
     public static String getUnsubscribeUrl(String host, String id) {
         return host + "/api/v1/registration/unsubscribe?id=" + id;
     }
